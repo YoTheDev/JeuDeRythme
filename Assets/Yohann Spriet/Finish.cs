60ab0a753c7668b653d08ad7a361d2120bcea546
YoTheDev <titoupriet@gmail.com>
2021-10-22 16:57:50 +0200

    Yohann Test Level design

---
+++ Assets/Yohann Spriet/Finish.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level design test");
+        }
+    }
+}
