1db13bb7c089d7c03e511a2d34156d93c4fc3c19
YoTheDev <titoupriet@gmail.com>
2021-11-19 23:21:21 +0100

    Test push

--- Assets/Adryan Hauton/Script/PlayerProperties.cs
+++ Assets/Adryan Hauton/Script/PlayerProperties.cs
@@ -12,21 +12,27 @@ public class PlayerProperties : MonoBehaviour
     public float JumpPower;
     public LevelScrolling LevelScrolling;
     public ParticleSystem Particles;
+    public float TeleportDistance;
 
     [SerializeField]private bool _isGrounded = false;
     private Rigidbody _rigidbody;
     private MeshRenderer _mesh;
+    public bool isTime = false;
+    public AudioSource Death;
 
     void Start()
     {
         _mesh = GetComponent<MeshRenderer>();
         _rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded && isTime)
         {
+            Debug.Log("hello");
             _rigidbody.AddForce(0, JumpPower, 0, ForceMode.Impulse);
         }
 
@@ -67,7 +73,12 @@ public class PlayerProperties : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Portal"))
         {
-            transform.position = new Vector3(0f, 1.3f, 20f);
+            transform.position = new Vector3(0f, 1.3f, TeleportDistance);
+        }
+
+        if (other.gameObject.CompareTag("FrontOfOb"))
+        {
+            isTime = true;
         }
         
         if (other.gameObject.CompareTag("Spikes"))
@@ -76,18 +87,28 @@ public class PlayerProperties : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("FrontOfOb"))
+        {
+            isTime = false;
+        }
+    }
+
     void PlayerDeath()
     {
         Destroy(_mesh);
         LevelScrolling._scrollingSpeed = 0f;
         Particles.Play();
-        //coupure de la musique
-        Invoke("ReloadScene", 1f);
+        Audio_manager.MusicLVL1.Stop();
+        Death.Play();
+        _rigidbody.constraints = RigidbodyConstraints.FreezePosition;
+        Invoke("ReloadScene", 2.5f);
     }
 
     void ReloadScene()
     {
-        LoadScene("AdryanH");
+        LoadScene("SceneLvl1");
     }
 
 }
